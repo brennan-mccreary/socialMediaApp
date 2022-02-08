ef65e3571852810b014ee6c4de86df3dfc6d705d
Brennan McCreary <brennanmccreary@gmail.com>
2022-02-07 22:18:16 -0500

    Initialize project, create react app, add basic file structure and dependencies.

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+//Imports
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+
+//Import components
+
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    //Run on component initial mount
+    componentDidMount() {
+
+    };
+
+    //Render components
+    render() {
+        return(
+            <div>
+                Hello
+            </div>
+        )
+    }
+}
+
+//Export
+export default App;
