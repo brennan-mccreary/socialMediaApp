20814720b2f05a14a23f9b62e0f54c11a3af048e
Nathaniel Brost <NathanielBrost@NathaniBook2021.attlocal.net>
2022-02-09 19:37:24 -0600

    working page nav and login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,16 @@
 //Imports
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios';
+// import axios from 'axios';
+import {
+    BrowserRouter, 
+    Routes, 
+    Route, 
+    Link
+} from "react-router-dom"
+
+import Login from "./Login/Login.jsx"
+
 
 //Import components
 
@@ -22,11 +31,44 @@ class App extends Component {
     //Render components
     render() {
         return(
-            <div>
-                Hello
-            </div>
+            <BrowserRouter>
+                {/* <div>
+                    Hello
+                </div> */}
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About Me</Link>
+                            </li>
+                            <li>
+                                <Link to="/create">Create Post</Link>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <Routes>
+                        <Route exact path="/about" element={<About />} />
+                        <Route exact path="/create" element={<Create />} />
+                        <Route exact path="/" element={<Login />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+
         )
     }
+}
+
+
+function About() {
+    return <h2>About to get Funky</h2>;
+}
+
+function Create() {
+    return <h2>Nooice</h2>;
 }
 
 //Export
---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Register = () => {
+    return (  
+        <div>
+        <h2>Home Boiii</h2>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">Email: </span>
+                <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+            </div>
+        
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon2">Password: </span>
+                <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+            </div>  
+    </div>                  
+
+    );
+}
+ 
+export default Register;
