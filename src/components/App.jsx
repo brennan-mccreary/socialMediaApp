732f2924932785e3030298c907714d12916ed200
Sabrina-Ibrahim <Sabrina.ibrahim24@me.com>
2022-02-14 21:44:13 -0600

    Functional login, begin logout function, and store register token.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import axios from 'axios';
 import Login from "./Login/Login.jsx"
 import Register from "./Register/Register.jsx"
+import jwtDecode from 'jwt-decode';
 import {
     BrowserRouter,
     Routes,
@@ -12,17 +13,22 @@ import {
 } from "react-router-dom"
 
 //Import components
-
-
 class App extends Component {
     constructor() {
         super();
         this.state = {
+            loginInfo: {
+                email: '',
+                password: ''
+            },
             registerInfo: {
                 firstName: '',
                 lastName: '',
                 email: '',
                 password: ''
+            },
+            currentUser: {
+
             }
         }
     }
@@ -39,8 +45,22 @@ class App extends Component {
 
     handleRegisterSubmit = (event) => {
         event.preventDefault();
-
         this.postRegister(this.state.registerInfo)
+    }
+
+    handleLoginChange = (event) => {
+        this.setState(prevState => ({
+            loginInfo: {
+                ...prevState.loginInfo,
+                [event.target.name]: event.target.value
+            }
+        })
+        )
+    }
+
+    handleLoginSubmit = (event) => {
+        event.preventDefault();
+        this.postLogin(this.state.loginInfo)
     }
 
     //HTTP Requests
@@ -48,8 +68,33 @@ class App extends Component {
         await axios
             .post('http://localhost:5003/api/users/register', info)
             .then((res) => {
-
+                console.log(res.headers["x-auth-token"])
+                localStorage.setItem("token", res.headers["x-auth-token"]);
+                const user = jwtDecode(localStorage.getItem("token"));
+                this.setState({
+                    currentUser: user
+                })
+                console.log(user)
             });
+    }
+
+    postLogin = async (info) => {
+        await axios
+            .post('http://localhost:5003/api/auth/', info)
+
+            .then((res) => {
+                console.log(res.data);
+                localStorage.setItem("token", res.data);
+                const user = jwtDecode(localStorage.getItem("token"));
+                this.setState({
+                    currentUser: user
+                })
+                console.log(user);
+            })
+    }
+
+    logOut = async () => {
+        localStorage.clear()
     }
 
     //Run on component initial mount
@@ -82,8 +127,9 @@ class App extends Component {
                     <Routes>
                         <Route exact path="/about" element={<About />} />
                         <Route exact path="/create" element={<Create />} />
+                        {/* <Route exact path="/login" element={<Login handleChange={this.handleRegisterChange} info={this.state.loginInfo} handleSubmit={this.handleLoginSubmit} />} /> */}
                         <Route exact path="/register" element={<Register handleChange={this.handleRegisterChange} info={this.state.registerInfo} handleSubmit={this.handleRegisterSubmit} />} />
-                        <Route exact path="/" element={<Login />} />
+                        <Route exact path="/" element={<Login handleChange={this.handleLoginChange} info={this.state.loginInfo} handleSubmit={this.handleLoginSubmit} />} />
                     </Routes>
                 </div>
             </BrowserRouter>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 
-const Login = () => {
-    return (  
+const Login = (props) => {
+    return (
         <div>
-        <h2>Logging</h2>
-            <div class="input-group mb-3">
-                <span class="input-group-text" id="basic-addon1">Email: </span>
-                <input type="text" class="form-control" placeholder="User Email" aria-label="User Email" aria-describedby="basic-addon1" />
-            </div>
-        
-            <div class="input-group mb-3">
-                <span class="input-group-text" id="basic-addon2">Password: </span>
-                <input type="text" class="form-control" placeholder="User Password" aria-label="User Password" aria-describedby="basic-addon2" />
-            </div>  
-    </div>                  
+            <h2>Logging</h2>
+            <form className="login" onSubmit={props.handleSubmit}>
+
+                <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon1">Email: </span>
+                    <input name='email' value={props.info.email} onChange={props.handleChange} type="text" className="form-control" aria-label="Email" aria-describedby="basic-addon1" />
+                </div>
+
+                <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon2">Password: </span>
+                    <input name='password' value={props.info.password} onChange={props.handleChange} type="text" className="form-control" aria-label="Password" aria-describedby="basic-addon1" />
+                </div>
+                <div>
+                    <button type="login" className="btn btn-primary">Login</button>
+                </div>
+            </form>
+        </div>
 
     );
 }
- 
+
 export default Login;
