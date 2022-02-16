809f7830c45d5a5990db7ba9b686c64581f1bc45
jasmiaharris <jazharris_31@yahoo.com>
2022-02-16 17:15:11 -0600

    Createdpost component and CreateAboutMe component.

---
+++ src/components/CreatePost/CreatePost.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Posts.css';
+
+const CreatePosts = (props) => {
+    return (
+        <div className='top-level' id= 'Post' hidden={true}>
+            Add a Post
+            <form className='post-box' onSubmit={props.submitPost}>
+                <input type="text"  value={props.postText} onChange={props.changePost}/>
+                <div className='button'>
+                    <button>Post</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default CreatePosts;
