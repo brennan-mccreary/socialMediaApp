064fbcd6306d80497da9b7de9d2031f630269fb3
Sabrina-Ibrahim <Sabrina.ibrahim24@me.com>
2022-02-20 16:29:38 -0600

    styling of personalposts and incoming friends.

--- src/components/IncomingFriends/IncomingFriends.jsx
+++ src/components/IncomingFriends/IncomingFriends.jsx
@@ -1,24 +1,24 @@
- import React from 'react';
- import './IncomingFriends.css';
+import React from 'react';
+import './IncomingFriends.css';
 
 const IncomingFriendRequests = (props) => {
     return (
         <div>
-            <div>
+            <div className='friend-request'>
                 <h2>Friend Requests</h2>
             </div>
             <div>
                 {(props.incoming.length > 0) ? props.incoming.map((el, i) =>
-                    <div className="container friend-request-container" key={i}>
+                    <div className="container" key={i}>
                         <div className="row">
                             <div className="col-xs-12 friend-request-list" >
                                 {el.firstName} {el.lastName}
                             </div>
                             <div className="col-lg-6">
-                                <button onClick={props.clickAccept} id={el._id}  className='btn btn-success '>Accept</button>
+                                <button onClick={props.clickAccept} id={el._id} className='btn btn-success'>Accept</button>
                             </div>
                             <div className="col-lg-6">
-                                <button onClick={props.clickDecline} id={el._id}  className='btn btn-danger'>Decline</button>
+                                <button onClick={props.clickDecline} id={el._id} className='btn btn-danger'>Decline</button>
                             </div>
                         </div>
                     </div>)
--- src/components/PostFeed/PostFeed.jsx
+++ src/components/PostFeed/PostFeed.jsx
@@ -5,13 +5,13 @@ const PostFeed = (props) => {
 
     return (
         <div>
-            {props.posts.length === 0 ? 
+            {props.posts.length === 0 ?
                 <div>
                     No posts to display...
                 </div>
                 :
                 <div>
-                    {props.posts.map((el, i) => 
+                    {props.posts.map((el, i) =>
                         <div className='card post-container' key={i}>
                             <div className="col">
                                 <div className="card" >
@@ -24,7 +24,7 @@ const PostFeed = (props) => {
                                         <button className='btn btn-success' onClick={props.handleClick} id={el._id}>Like</button>
                                         <h6>Likes: {el.likeCount}</h6>
                                     </div>
-                                    
+
                                 </div>
                             </div>
                         </div>
